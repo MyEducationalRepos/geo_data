9bc2530c21a19f063fbdaf866cfc4b676fecbea5
Germán Goñi <germangoni@icloud.com>
2023-01-09 20:04:02 -0300

    First commit, including main

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+import pandas as pd
+from requests.auth import HTTPBasicAuth
+import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
+'''
+
+Note that credentials are read from an .env file
+This should be obtained from RapidAPI
+
+'''
+
+
+api_token = os.environ.get("RAPID_API_KEY")
+
+import requests
+
+url = "https://google-maps-geocoding.p.rapidapi.com/geocode/json"
+
+#A case
+
+querystring = {"address":"164 Townsend St., San Francisco, CA","language":"en"}
+
+headers = {
+	"X-RapidAPI-Key": api_token,
+	"X-RapidAPI-Host": "google-maps-geocoding.p.rapidapi.com"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
