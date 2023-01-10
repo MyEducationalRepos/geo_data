fc1b88ea2daa586087970841a29b7bcfbe1b9cc6
Germán Goñi <germangoni@icloud.com>
2023-01-10 13:25:44 -0300

    Changed with list of different location / adresses

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import pandas as pd
 from requests.auth import HTTPBasicAuth
 import json
 import os
+import sys
 from dotenv import load_dotenv
 load_dotenv()
-
 
 
 '''
@@ -18,19 +18,24 @@ This should be obtained from RapidAPI
 
 api_token = os.environ.get("RAPID_API_KEY")
 
-import requests
 
 url = "https://google-maps-geocoding.p.rapidapi.com/geocode/json"
 
-#A case
-
-querystring = {"address":"14 Norte 821, Viña Del Mar","language":"es"}
 
 headers = {
 	"X-RapidAPI-Key": api_token,
 	"X-RapidAPI-Host": "google-maps-geocoding.p.rapidapi.com"
 }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
 
-print(response.text)
+direcciones = ["14 Norte 221, Viña del Mar","Chanchería Chile"]
+    
+for _, direccion in enumerate(direcciones):    
+    querystring = {"address":direccion,"language":"es"}
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    print(response.text)
+
+
+
+
+
